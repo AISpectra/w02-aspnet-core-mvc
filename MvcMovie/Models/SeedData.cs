6bdbe74bac1440f7bf4dfcde1df1d74c925ec400
AISpectra <spectra.gethelp@gmail.com>
2026-05-15 13:27:25 +0200

    new

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -22,28 +22,32 @@ public static class SeedData
                     Title = "Interstellar",
                     ReleaseDate = DateTime.Parse("2014-11-07"),
                     Genre = "Science Fiction",
-                    Price = 9.99M
+                    Price = 9.99M,
+                    Rating = "PG-13"
                 },
                 new Movie
                 {
                     Title = "Gladiator",
                     ReleaseDate = DateTime.Parse("2000-05-05"),
                     Genre = "Action",
-                    Price = 8.99M
+                    Price = 8.99M,
+                    Rating = "PG-13"
                 },
                 new Movie
                 {
                     Title = "The Dark Knight",
                     ReleaseDate = DateTime.Parse("2008-07-18"),
                     Genre = "Action",
-                    Price = 10.99M
+                    Price = 10.99M,
+                    Rating = "PG-13"
                 },
                 new Movie
                 {
                     Title = "Inception",
                     ReleaseDate = DateTime.Parse("2010-07-16"),
                     Genre = "Science Fiction",
-                    Price = 9.49M
+                    Price = 9.49M,
+                    Rating = "PG-13"
                 }
             );
 
